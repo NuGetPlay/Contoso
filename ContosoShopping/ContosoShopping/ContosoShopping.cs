439924fc9d2d2ae20426a18d9afc3897ff61cda4
Anand Gaurav <agaurav.edu@live.com>
2017-05-01 11:25:13 -0700

    Project added

---
+++ ContosoShopping/ContosoShopping/ContosoShopping.cs
@@ -0,0 +1,19 @@
+﻿using Contoso.Base;
+using System;
+
+namespace ContosoShopping
+{
+    public class ContosoShopping
+    {
+        public static string GetVersion()
+        {
+            return "Contoso Shopping 1.0.0 (NETStandard|net452)";
+        }
+
+        public static ShopCart GetShoppingCart(Guid userId)
+        {
+            ShopCart shopCart = new ShopCart(userId);
+            return shopCart;
+        }
+    }
+}
