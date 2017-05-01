9d85ecdf4d110d4b7c9fed111ef340d5b955f05d
Anand Gaurav <agaurav.edu@live.com>
2017-05-01 12:55:44 -0700

    Added 1.0.2

--- ContosoShopping/ContosoShopping/ContosoShopping.cs
+++ ContosoShopping/ContosoShopping/ContosoShopping.cs
@@ -7,13 +7,13 @@ namespace ContosoShopping
     {
         public static string GetVersion()
         {
-            return "Contoso Shopping 1.0.0 (NETStandard|net452)";
+            return "Contoso Shopping 1.0.2 (NETStandard|net452)";
         }
-
         public static ShopCart GetShoppingCart(Guid userId)
         {
             ShopCart shopCart = new ShopCart(userId);
             return shopCart;
         }
+
     }
 }
