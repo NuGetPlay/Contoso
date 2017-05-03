a751e992b740fbcb62e19dfd589e2b574fb31c3f
Anand Gaurav <agaurav.edu@live.com>
2017-05-03 11:10:34 -0700

    1.1.0

--- ContosoShopping/ContosoShopping/Class1.cs
+++ ContosoShopping/ContosoShopping/Class1.cs
@@ -3,11 +3,11 @@ using System;
 
 namespace ContosoShopping
 {
-    public class ContosoShopping
+    public class Class1
     {
         public static string GetVersion()
         {
-            return "Contoso Shopping 1.0.5 (NETStandard)";
+            return "Contoso Shopping 1.0.0 (NETStandard)";
         }
 
         public static ShopCart GetShoppingCart(Guid userId)
